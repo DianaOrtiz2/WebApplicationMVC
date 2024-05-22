5e39200fe388967c972554a3a7bc333e3ae265a2
Diana Ortiz <al44776@un.edu.mx>
2024-05-21 18:31:00 -0600

    cambios Bootstrap.

--- obj/Debug/net8.0/WebApplicationMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplicationMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83e3eb41bf22fabfe8d05547957b2eab61fbfc22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f802b8e605d0e1c52aa10e3d4184fba5687810a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
